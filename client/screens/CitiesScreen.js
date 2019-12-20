7615573bd5245cb060ef96ff9df8f2f8437e2154
Greg Larsen <gslarsen2718@gmail.com>
2019-12-20 11:52:55 -0500

    add scrollview to location details and some styling

---
+++ client/screens/CitiesScreen.js
@@ -0,0 +1,110 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+  ImageBackground
+} from "react-native";
+
+import { CITIES } from "../data/data";
+import Colors from "../constants/Colors";
+
+const CitiesScreen = props => {
+  const renderListItem = data => {
+    return (
+      <TouchableOpacity
+        style={styles.listItem}
+        onPress={() =>
+          props.navigation.navigate("Tours", {
+            id: data.item.id,
+            name: data.item.name
+          })
+        }
+      >
+        <View>
+          <Text style={styles.text}>{data.item.name}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View>
+        <ImageBackground
+          style={styles.image}
+          source={require("../assets/hero.jpeg")}
+        >
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>Tasty Tours</Text>
+          </View>
+        </ImageBackground>
+      </View>
+      <FlatList
+        keyExtractor={(item, idx) => item.id}
+        data={CITIES}
+        renderItem={renderListItem}
+        numColumns={1}
+      />
+    </View>
+  );
+};
+
+CitiesScreen.navigationOptions = {
+  headerTitle: "TASTY TOURS",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+  },
+  headerTitleStyle: {
+    fontFamily: "open-sans-bold",
+    textAlign: 'center'
+  },  
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
+};
+
+const styles = StyleSheet.create({
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 80,
+    width: "60%",
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: Colors.accentColor,
+    borderRadius: 7
+  },
+  text: {
+    color: Colors.primaryColor,
+    fontFamily: "open-sans-bold",
+    textAlign: "center",
+    fontSize: 20
+  },
+  image: {
+    width: 400,
+    height: 200,
+    marginTop: 10,
+    marginBottom: 15,
+    marginLeft: 7,
+    borderRadius: 10,
+    justifyContent: "center"
+  },
+  titleContainer: {
+    backgroundColor: "rgba(0,0,0,0.2)",
+    paddingVertical: 5,
+    paddingHorizontal: 12
+  },
+  title: {
+    fontFamily: "lobster",
+    fontSize: 50,
+    color: "white",
+    textAlign: "center"
+  },
+  headerButton: {
+    color: "blue"
+  }
+});
+
+export default CitiesScreen;
---
+++ client/screens/LocationDetailsScreen.js
@@ -0,0 +1,105 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  Platform,
+  Image
+} from "react-native";
+import ViewMoreText from "react-native-view-more-text";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import LocationHeader from "../components/LocationHeader";
+import HeaderButton from "../components/HeaderButton";
+import { TOURS } from "../data/data";
+import Colors from "../constants/Colors";
+
+const LocationDetailsScreen = props => {
+  const tourId = props.navigation.getParam("tourId");
+  const locName = props.navigation.getParam("name");
+  const place_id = props.navigation.getParam("place_id");
+  const menu = props.navigation.getParam("menu");
+
+  const renderViewMore = onPress => {
+    return <Text onPress={onPress}>View more</Text>;
+  };
+
+  const renderViewLess = onPress => {
+    return <Text onPress={onPress}>View less</Text>;
+  };
+
+  const tour = TOURS.find(item => item.id === tourId);
+
+  const location = tour.locations.find(
+    location => location.locationName === locName
+  );
+  
+  return (
+    <View>
+    <ScrollView style={styles.summary}>
+      <Image style={styles.image} source={{ uri: location.image }} />
+      
+      <LocationHeader style={styles.header} place_id={place_id} menu={menu} />
+      
+        <ViewMoreText
+          numberOfLines={17}
+          renderViewMore={this.renderViewMore}
+          renderViewLess={this.renderViewLess}
+          textStyle={{}}
+        >
+          <Text style={styles.text}>{location.summary}</Text>
+        </ViewMoreText>
+        <View><Text style={{color: 'white'}}>-</Text></View>
+      </ScrollView>
+    </View>
+  );
+};
+
+LocationDetailsScreen.navigationOptions = navData => {
+  // console.log('NAV OPTIONS:', navData.navigation.getParam('name'))
+  const locName = navData.navigation.getParam("name");
+
+  return {
+    headerTitle: `${locName}`,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : ""
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Home"
+          iconName="md-home"
+          onPress={() => {
+            navData.navigation.navigate("Cities");
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
+};
+
+const styles = StyleSheet.create({
+  summary: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    padding: 10
+  },
+  text: {
+    fontFamily: "open-sans",
+    textAlign: "justify"
+  },
+  image: {
+    marginTop: 15,
+    width: 400,
+    height: 200,
+    borderRadius: 10,
+    marginLeft: 6
+  },
+  header: {
+    marginTop: 50
+  }
+});
+
+export default LocationDetailsScreen;
